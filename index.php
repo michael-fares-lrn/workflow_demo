caa1825b222f09161f1b63a037bf5caf5fe8cd25
michael-fares-lrn <michael.fares@learnosity.com>
2024-07-11 14:09:13 -0700

    test commit with new computer

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     </title>
 </head>
 <body>
-    <h2>Here is a hands on demo showing what Learnosity APIs can do!</h2>
+    <h2>Here is a hands on demo showing what Learnosity APIs can do! Updated July 11 2024</h2>
   
         <span>First stop:</span>
         <a href="./author.php">
